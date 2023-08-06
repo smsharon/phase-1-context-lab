ff8ef91408ce236430d823953dff7627cec61c87
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-08-06 15:19:11 -0400

    add some functions

--- index.js
+++ index.js
@@ -1,5 +1,74 @@
 /* Your Code Here */
+// an empty array to store employee records
+let employeeRecords = [];
+// Function to create an employee record
+function createEmployeeRecord(employeeData) {
+    return {
+      firstName: employeeData[0],
+      familyName: employeeData[1],
+      title: employeeData[2],
+      payPerHour: employeeData[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  // Function to create employee records from an array of arrays
+function createEmployeeRecords(arraysOfData) {
+    return arraysOfData.map(createEmployeeRecord);
+  }
+  // Function to create a time-in event
+function createTimeInEvent(employee, timeStamp) {
+    const [date, hour] = timeStamp.split(" ");
+    employee.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date: date
+    });
+    return employee;
+  }
+  // Function to create a time-out event
+function createTimeOutEvent(employee, timeStamp) {
+    const [date, hour] = timeStamp.split(" ");
+    employee.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date: date
+    });
+    return employee;
+  }
+  // Function to calculate hours worked on a specific date
+function hoursWorkedOnDate(employee, date) {
+    const timeIn = employee.timeInEvents.find(event => event.date === date);
+    const timeOut = employee.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  // Function to calculate wages earned on a specific date
+function wagesEarnedOnDate(employee, date) {
+    const hoursWorked = hoursWorkedOnDate(employee, date);
+    return hoursWorked * employee.payPerHour;
+  }
+// Function to find an employee record by first name
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+  }
 
+// Function to calculate payroll for all employees
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPayroll, employee) => totalPayroll + allWagesFor(employee), 0);
+  }
+  // Create employee records
+employeeRecords = createEmployeeRecords(employeeData);
+
+// Punch in and out for employees
+createTimeInEvent(employeeRecords[0], "2023-08-06 0900");
+createTimeOutEvent(employeeRecords[0], "2023-08-06 1700");
+createTimeInEvent(employeeRecords[1], "2023-08-06 1000");
+createTimeOutEvent(employeeRecords[1], "2023-08-06 1600");
+
+// Calculate total wages for all employees
+const totalPayroll = calculatePayroll(employeeRecords);
+
+console.log("Total Payroll:", totalPayroll);
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
